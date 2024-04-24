46b54b72f5e225e2a0ca7f4076915b57283151e3
Ankush Menat <ankush@frappe.io>
2024-04-24 20:09:20 +0530

    fix!: Overriden doctypes must inherit same base class
    There is almost never a real need to completely override a class. After
    this change we'll only allow extending and not overriding completely.

--- frappe/model/base_document.py
+++ frappe/model/base_document.py
@@ -87,16 +87,22 @@ def import_controller(doctype):
 
 	module_path = None
 	class_overrides = frappe.get_hooks("override_doctype_class")
+
+	module = load_doctype_module(doctype, module_name)
+	classname = doctype.replace(" ", "").replace("-", "")
+	class_ = getattr(module, classname, None)
+
 	if class_overrides and class_overrides.get(doctype):
 		import_path = class_overrides[doctype][-1]
-		module_path, classname = import_path.rsplit(".", 1)
-		module = frappe.get_module(module_path)
+		module_path, custom_classname = import_path.rsplit(".", 1)
+		custom_module = frappe.get_module(module_path)
+		custom_class_ = getattr(custom_module, custom_classname, None)
+		if not issubclass(custom_class_, class_):
+			raise ImportError(
+				f"{doctype}: Invalid override, {classname} is not a subclass of {class_.__name__}"
+			)
+		class_ = custom_class_
 
-	else:
-		module = load_doctype_module(doctype, module_name)
-		classname = doctype.replace(" ", "").replace("-", "")
-
-	class_ = getattr(module, classname, None)
 	if class_ is None:
 		raise ImportError(
 			doctype
